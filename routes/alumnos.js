b3ad8994f99bb7e5613e958b203400b2d96d12f5
M4RK <yoldimarcos@gmail.com>
2022-12-14 20:18:07 -0300

    query modified

--- routes/alumnos.js
+++ routes/alumnos.js
@@ -41,7 +41,10 @@ db = mysql.createPool({
 /* Alumnos */
 //get Alumnos
 const getAlumnos = (req, res, next) => {
-  let sql = `SELECT IF(nota >= 3 , 'PROMOCIONADO' , 'DESAPROBADO') condicion, alumnos.id, alumnos.nombre, alumnos.apellido, alumnos.anio, materias.materia FROM notas , materias , alumnos WHERE notas.id_materia = materias.id AND notas.id_Alumno = alumnos.id;`;
+  let sql = `SELECT id_alumno, alumnos.nombre, alumnos.apellido, alumnos.anio, COUNT(*) as materias_aprobadas FROM notas
+            left join alumnos
+            on  notas.id_alumno = alumnos.id
+            WHERE (notas.nota >= 6) GROUP BY notas.id_alumno;`
   connection.query(sql, function (err, data, fields) {
       if (err) throw err;
       res.json(data)
